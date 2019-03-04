aa5a8d3853d471a5fe5b57d8ad5cac1e77aee3f4
Ben Bradley <bradleybj09@gmail.com>
2019-03-04 11:59:19 -0500

    finishing dagger setup

--- app/src/main/java/com/example/nationalparks/dagger/AppComponent.kt
+++ app/src/main/java/com/example/nationalparks/dagger/AppComponent.kt
@@ -1,13 +1,13 @@
 package com.example.nationalparks.dagger
 
-import android.app.Application
+import com.example.nationalparks.util.NationalParksApplication
 import dagger.Component
 import dagger.android.AndroidInjectionModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AndroidInjectionModule::class, AppModule::class, BuildersModule::class])
+@Component(modules = [AndroidInjectionModule::class, BuildersModule::class, AppModule::class])
 interface AppComponent {
 
-    fun inject(application: Application)
+    fun inject(application: NationalParksApplication)
 }
